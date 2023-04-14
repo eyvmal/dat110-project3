834ec90f2534bf65ca84a2f8acbd166c04b38f0e
eyvmal <eyvind.malde@gmail.com>
2023-04-14 16:03:49 +0200

    finished task1

--- src/main/java/no/hvl/dat110/util/Hash.java
+++ src/main/java/no/hvl/dat110/util/Hash.java
@@ -6,7 +6,6 @@ package no.hvl.dat110.util;
  *
  */
 
-import java.io.UnsupportedEncodingException;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -17,33 +16,40 @@ public class Hash {
 	public static BigInteger hashOf(String entity) {	
 		
 		BigInteger hashint = null;
-		
+
 		// Task: Hash a given string using MD5 and return the result as a BigInteger.
-		
-		// we use MD5 with 128 bits digest
-		
-		// compute the hash of the input 'entity'
-		
-		// convert the hash into hex format
-		
-		// convert the hex into BigInteger
-		
+		try {
+			// we use MD5 with 128 bits digest
+			MessageDigest md = MessageDigest.getInstance("MD5");
+
+			// compute the hash of the input 'entity'
+			byte[] hash = md.digest(entity.getBytes());
+
+			// convert the hash into hex format
+			String hex = toHex(hash);
+
+			// convert the hex into BigInteger
+			hashint = new BigInteger(hex, 16);
+
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+
 		// return the BigInteger
-		
 		return hashint;
 	}
 	
 	public static BigInteger addressSize() {
 		
 		// Task: compute the address size of MD5
-		
 		// compute the number of bits = bitSize()
+		int bits = bitSize();
 		
 		// compute the address size = 2 ^ number of bits
-		
+		BigInteger addressSize = BigInteger.valueOf(2).pow(bits);
+
 		// return the address size
-		
-		return null;
+		return addressSize;
 	}
 	
 	public static int bitSize() {
@@ -51,7 +57,13 @@ public class Hash {
 		int digestlen = 0;
 		
 		// find the digest length
-		
+		try {
+			MessageDigest md = MessageDigest.getInstance("MD5");
+			digestlen = md.getDigestLength();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+
 		return digestlen*8;
 	}
 	
